48035e3b0ee13d9bdb96fcfecc735e7b0ee3e0e7
woniu17 <qinglucklin@foxmail.com>
2015-03-29 17:52:08 +0800

    mod diff.sh

--- sim/simulation/tmp/dctcpdiff/diff.sh
+++ sim/simulation/tmp/dctcpdiff/diff.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 echo "diff -crbB ns-default.tcl dctcp-ns-default.tcl"
 diff -crbB ns-default.tcl dctcp-ns-default.tcl
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@l@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "diff -crbB red.cc dctcp-red.cc"
 diff -crbB red.cc dctcp-red.cc
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@l@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "diff -crbB tcp.cc dctcp-tcp.cc"
 diff -crbB tcp.cc dctcp-tcp.cc
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@l@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "diff -crbB tcp.h dctcp-tcp.h"
 diff -crbB tcp.h dctcp-tcp.h
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@l@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "diff -crbB tcp-full.cc dctcp-tcp-full.cc"
 diff -crbB tcp-full.cc dctcp-tcp-full.cc
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@l@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "diff -crbB tcp-full.h dctcp-tcp-full.h"
 diff -crbB tcp-full.h dctcp-tcp-full.h
