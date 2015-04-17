65ff6f8278eedf57e60cc46336b643694d832532
woniu17 <qinglucklin@foxmail.com>
2015-04-17 14:00:00 +0800

    add sdn patch to ns2

--- sim/simulation/tmp/ns2-patch/ns2-home-2-patch.sh
+++ sim/simulation/tmp/ns2-patch/ns2-home-2-patch.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
-if [ "$2"x = x ]; then
-MODIFY="modify"
+#!/bin/bash
+if [ "$1"x = x ]; then
+MODIFY="sdn"
 else
-MODIFY=$2
+MODIFY=$1
 fi
+
+echo "move ns-home to  $MODIFY"
+
 NS_HOME=~/bin/ns-allinone-2.35/ns-2.35
 cp $NS_HOME/queue/red.cc $MODIFY-red.cc
 cp $NS_HOME/tcp/tcp.cc $MODIFY-tcp.cc
--- sim/simulation/tmp/ns2-patch/patch-2-ns2-home.sh
+++ sim/simulation/tmp/ns2-patch/patch-2-ns2-home.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+if [ "$1"x = x ]; then
+MODIFY="sdn"
+else
+MODIFY=$1
+fi
+
+echo "move $MODIFY to ns home"
+
 NS_HOME=~/bin/ns-allinone-2.35/ns-2.35
-cp d2tcp-red.cc $NS_HOME/queue/red.cc
-cp d2tcp-tcp.cc $NS_HOME/tcp/tcp.cc
-cp d2tcp-tcp.h $NS_HOME/tcp/tcp.h
-cp d2tcp-tcp-full.cc $NS_HOME/tcp/tcp-full.cc
-cp d2tcp-tcp-full.h $NS_HOME/tcp/tcp-full.h
+cp $MODIFY-red.cc $NS_HOME/queue/red.cc
+cp $MODIFY-tcp.cc $NS_HOME/tcp/tcp.cc
+cp $MODIFY-tcp.h $NS_HOME/tcp/tcp.h
+cp $MODIFY-tcp-full.cc $NS_HOME/tcp/tcp-full.cc
+cp $MODIFY-tcp-full.h $NS_HOME/tcp/tcp-full.h
